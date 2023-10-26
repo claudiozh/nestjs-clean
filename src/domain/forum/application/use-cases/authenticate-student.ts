84e37b60f8431bcd6c55d98bb47a814fb164cd2d
claudiozh <claudiorodrigozh@gmail.com>
2023-10-26 16:31:57 -0300

    feat: added use case to register student and authenticate student

---
+++ src/domain/forum/application/use-cases/authenticate-student.ts
@@ -0,0 +1,49 @@
+import { WrongCredentialsError } from '@/domain/forum/application/use-cases/errors/wrong-credentials-error';
+import { Either, left, right } from '@/core/either';
+import { Injectable } from '@nestjs/common';
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository';
+import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer';
+import { Encrypter } from '@/domain/forum/application/cryptography/encrypter';
+
+interface AuthenticateStudentUseCaseRequest {
+  email: string;
+  password: string;
+}
+
+type AuthenticateStudentUseCaseResponse = Either<
+  WrongCredentialsError,
+  {
+    accessToken: string;
+  }
+>;
+
+@Injectable()
+export class AuthenticateStudentUseCase {
+  constructor(
+    private readonly studentsRepository: StudentsRepository,
+    private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter,
+  ) {}
+
+  async execute({ email, password }: AuthenticateStudentUseCaseRequest): Promise<AuthenticateStudentUseCaseResponse> {
+    const student = await this.studentsRepository.findByEmail(email);
+
+    if (!student) {
+      return left(new WrongCredentialsError());
+    }
+
+    const passwordMatch = await this.hashComparer.compare(password, student.password);
+
+    if (!passwordMatch) {
+      return left(new WrongCredentialsError());
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: student.id.toString(),
+    });
+
+    return right({
+      accessToken,
+    });
+  }
+}
---
+++ src/domain/forum/application/use-cases/register-student.ts
@@ -0,0 +1,46 @@
+import { Either, left, right } from '@/core/either';
+import { Injectable } from '@nestjs/common';
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository';
+import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator';
+import { StudentAlreadyExistsError } from './errors/student-already-exists-error';
+import { Student } from '@/domain/forum/enterprise/entities/student';
+
+interface RegisterStudentUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+type RegisterStudentUseCaseResponse = Either<
+  StudentAlreadyExistsError,
+  {
+    student: Student;
+  }
+>;
+
+@Injectable()
+export class RegisterStudentUseCase {
+  constructor(private readonly studentsRepository: StudentsRepository, private readonly hashGenerator: HashGenerator) {}
+
+  async execute({ name, email, password }: RegisterStudentUseCaseRequest): Promise<RegisterStudentUseCaseResponse> {
+    const studentWithSameEmail = await this.studentsRepository.findByEmail(email);
+
+    if (studentWithSameEmail) {
+      return left(new StudentAlreadyExistsError(email));
+    }
+
+    const passwordHash = await this.hashGenerator.generate(password);
+
+    const student = Student.create({
+      name,
+      email,
+      password: passwordHash,
+    });
+
+    await this.studentsRepository.create(student);
+
+    return right({
+      student,
+    });
+  }
+}
