cd1e0c990eb981fdd36d60693ac627a2ef042779
claudiozh <claudiorodrigozh@gmail.com>
2023-11-06 17:10:32 -0300

    refactor: ajusted test

--- src/domain/forum/application/use-cases/register-student.spec.ts
+++ src/domain/forum/application/use-cases/register-student.spec.ts
@@ -1,37 +1,43 @@
+import { RegisterStudentUseCase } from './register-student';
 import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository';
-import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student';
 import { FakeHasher } from 'test/cryptography/fake-hasher';
-import { makeStudent } from 'test/factories/make-student';
 
 let inMemoryStudentsRepository: InMemoryStudentsRepository;
 let fakeHasher: FakeHasher;
+
 let sut: RegisterStudentUseCase;
 
 describe('Register Student', () => {
   beforeEach(() => {
     inMemoryStudentsRepository = new InMemoryStudentsRepository();
     fakeHasher = new FakeHasher();
+
     sut = new RegisterStudentUseCase(inMemoryStudentsRepository, fakeHasher);
   });
 
-  it('should be able to register student', async () => {
-    const student = makeStudent();
-    const result = await sut.execute(student);
+  it('should be able to register a new student', async () => {
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
 
     expect(result.isRight()).toBe(true);
-    expect(inMemoryStudentsRepository.items.length).toBe(1);
     expect(result.value).toEqual({
       student: inMemoryStudentsRepository.items[0],
     });
   });
 
   it('should hash student password upon registration', async () => {
-    const student = makeStudent();
-    const result = await sut.execute(student);
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
 
-    const hashedPassword = await fakeHasher.hash(student.password);
+    const hashedPassword = await fakeHasher.hash('123456');
 
     expect(result.isRight()).toBe(true);
-    expect(inMemoryStudentsRepository.items[0].password).toEqual(hashedPassword);
+    expect(inMemoryStudentsRepository.items[0].passwordHash).toEqual(hashedPassword);
   });
 });
