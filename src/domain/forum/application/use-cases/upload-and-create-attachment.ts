40b772123d0818f9434554aab075ca44fa4e0852
claudiozh <claudiorodrigozh@gmail.com>
2023-11-07 11:06:20 -0300

    feat: added use casse to upload attachment

---
+++ src/domain/forum/application/use-cases/upload-and-create-attachment.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+import { Either, left, right } from '@/core/either';
+import { InvalidAttachmentError } from '@/domain/forum/application/use-cases/errors/invalid-attachment-type-error';
+import { Attachment } from '@/domain/forum/enterprise/entities/attachment';
+import { AttachmentsRepository } from '@/domain/forum/application/repositories/attachments-repository';
+import { Uploader } from '@/infra/storage/uploader';
+
+interface UploadAndCreateAttachmentUseCaseRequest {
+  fileName: string;
+  fileType: string;
+  body: Buffer;
+}
+
+type UploadAndCreateAttachmentUseCaseResponse = Either<
+  InvalidAttachmentError,
+  {
+    attachment: Attachment;
+  }
+>;
+
+@Injectable()
+export class UploadAndCreateAttachmentUseCase {
+  constructor(private readonly uploader: Uploader, private readonly attachmentsRepository: AttachmentsRepository) {}
+
+  async execute({
+    fileName,
+    fileType,
+    body,
+  }: UploadAndCreateAttachmentUseCaseRequest): Promise<UploadAndCreateAttachmentUseCaseResponse> {
+    const regexMimeType = /^image\/(png|jpg|jpeg|pdf)$/;
+
+    if (!regexMimeType.test(fileType)) {
+      return left(new InvalidAttachmentError(fileType));
+    }
+
+    const { url } = await this.uploader.upload({
+      fileName,
+      fileType,
+      body,
+    });
+
+    const attachment = Attachment.create({
+      title: fileName,
+      url,
+    });
+
+    await this.attachmentsRepository.create(attachment);
+
+    return right({
+      attachment,
+    });
+  }
+}
