f6ea7f900ccba4f6c233e80aa0b47d7171720562
claudiozh <claudiorodrigozh@gmail.com>
2023-11-13 10:09:27 -0300

    refactor: edit use case to edit question

--- src/infra/http/controllers/edit-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/edit-question.controller.e2e-spec.ts
@@ -1,3 +1,5 @@
+import { QuestionAttachmentFactory } from './../../../../test/factories/make-question-attachments';
+import { AttachmentFactory } from 'test/factories/make-attachment';
 import { INestApplication } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Test } from '@nestjs/testing';
@@ -13,19 +15,28 @@ describe('Edit question (E2E)', () => {
   let prismaService: PrismaService;
   let studentFactory: StudentFactory;
   let questionFactory: QuestionFactory;
+  let attachmentFactory: AttachmentFactory;
+  let questionAttachmentFactory: QuestionAttachmentFactory;
 
   let jwtService: JwtService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
-      providers: [StudentFactory, QuestionFactory],
+      providers: [
+        StudentFactory,
+        QuestionFactory,
+        AttachmentFactory,
+        QuestionAttachmentFactory,
+      ],
     }).compile();
 
     app = moduleRef.createNestApplication();
     prismaService = moduleRef.get(PrismaService);
     studentFactory = moduleRef.get(StudentFactory);
     questionFactory = moduleRef.get(QuestionFactory);
+    attachmentFactory = moduleRef.get(AttachmentFactory);
+    questionAttachmentFactory = moduleRef.get(QuestionAttachmentFactory);
     jwtService = moduleRef.get(JwtService);
 
     await app.init();
@@ -38,9 +49,24 @@ describe('Edit question (E2E)', () => {
       sub: user.id.toString(),
     });
 
+    const attachment1 = await attachmentFactory.makePrismaAttachment();
+    const attachment2 = await attachmentFactory.makePrismaAttachment();
+
     const question = await questionFactory.makePrismaQuestion({
       authorId: user.id,
     });
+
+    await questionAttachmentFactory.makePrismaQuestionAttachment({
+      attachmentId: attachment1.id,
+      questionId: question.id,
+    });
+
+    await questionAttachmentFactory.makePrismaQuestionAttachment({
+      attachmentId: attachment2.id,
+      questionId: question.id,
+    });
+
+    const attachment3 = await attachmentFactory.makePrismaAttachment();
 
     const questionId = question.id;
 
@@ -49,6 +75,7 @@ describe('Edit question (E2E)', () => {
       .send({
         title: 'New title',
         content: 'New content',
+        attachments: [attachment1.id.toString(), attachment3.id.toString()],
       })
       .set('Authorization', `Bearer ${accessToken}`);
 
@@ -62,5 +89,23 @@ describe('Edit question (E2E)', () => {
     });
 
     expect(questionOnDatabase).toBeTruthy();
+
+    const attachmentsOnDatabase = await prismaService.attachment.findMany({
+      where: {
+        questionId: questionOnDatabase?.id,
+      },
+    });
+
+    expect(attachmentsOnDatabase).toHaveLength(2);
+    expect(attachmentsOnDatabase).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: attachment1.id.toString(),
+        }),
+        expect.objectContaining({
+          id: attachment3.id.toString(),
+        }),
+      ]),
+    );
   });
 });
--- src/infra/http/controllers/edit-question.controller.ts
+++ src/infra/http/controllers/edit-question.controller.ts
@@ -16,6 +16,7 @@ import { z } from 'zod';
 const editQuestionBodySchema = z.object({
   title: z.string(),
   content: z.string(),
+  attachments: z.array(z.string().uuid()),
 });
 
 type EditQuestionBody = z.infer<typeof editQuestionBodySchema>;
@@ -31,14 +32,14 @@ export class EditQuestionController {
     @AuthUser() user: IAuthUser,
     @Param('id') questionId: string,
   ) {
-    const { title, content } = body;
+    const { title, content, attachments } = body;
     const userId = user.sub;
 
     const result = await this.editQuestion.execute({
       title,
       content,
       authorId: userId,
-      attachmentsIds: [],
+      attachmentsIds: attachments,
       questionId,
     });
 
